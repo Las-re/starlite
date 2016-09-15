0c0a3f9c68118bf49e4a7b3ba518916b47fa024a
Lynn Owens <Lynn Owens>
2016-09-15 15:57:55 -0500

    several updates to bring into bean standards and rename for clarity

---
+++ net/tofweb/jdstarlite/CellInfo.java
@@ -0,0 +1,56 @@
+package net.tofweb.jdstarlite;
+
+/*
+ * @author Lynn Owens
+ * https://github.com/LynnOwens
+ */
+public class CellInfo {
+	private double g;
+	private double rhs;
+	private double cost;
+	
+	public CellInfo() {
+		super();
+		this.cost = DStarLite.DEFAULT_CELL_COST;
+	}
+
+	/**
+	 * Cost thus far from the start to current node
+	 * 
+	 * @return
+	 */
+	public double getG() {
+		return g;
+	}
+
+	public void setG(double g) {
+		this.g = g;
+	}
+
+	/**
+	 * Right Hand Side, heuristic.  g + estimated cost to move to this cell
+	 * 
+	 * @return
+	 */
+	public double getRhs() {
+		return rhs;
+	}
+
+	public void setRhs(double rhs) {
+		this.rhs = rhs;
+	}
+
+	/**
+	 * The cost of the cell
+	 * 
+	 * @return
+	 */
+	public double getCost() {
+		return cost;
+	}
+
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+
+}
---
+++ net/tofweb/jdstarlite/IPoint2.java
@@ -0,0 +1,36 @@
+package net.tofweb.jdstarlite;
+
+/*
+ * @author Lynn Owens
+ * https://github.com/LynnOwens
+ */
+public class IPoint2 {
+	private int x = 0;
+	private int y = 0;
+
+	public IPoint2() {
+
+	}
+
+	public IPoint2(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+}
