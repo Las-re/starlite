dcbef0e3f7d6eab5d6f31dee35106d94c4d85e36
Lynn <Lynn@WithMushLove>
2016-09-26 18:35:03 -0500

    Added end-to-end test case for future refactoring

--- src/test/java/net/tofweb/starlite/Integration.java
+++ src/test/java/net/tofweb/starlite/Integration.java
@@ -1,6 +1,10 @@
 package net.tofweb.starlite;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
 
 import org.junit.Test;
 
@@ -8,7 +12,34 @@ public class Integration {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		DStarLite dsl = new DStarLite();
+
+		// set start and goal nodes
+		dsl.init(10, 7, 7, 1, 1, -1);
+
+		// set impassable nodes
+		dsl.updateCell(6, 6, 6, -1);
+		dsl.updateCell(2, 1, 1, -1);
+		dsl.updateCell(2, 2, 1, -1);
+
+		// perform the pathfinding
+		dsl.pathfind();
+
+		// get and print the path
+		List<State> path = dsl.getPath();
+		for (State state : path) {
+			System.out.println("x: " + state.getX() + " y: " + state.getY() + " z: " + state.getZ());
+		}
+
+		assertNotNull(path);
+
+		Integer size = path.size();
+		assertTrue(24 == size);
+
+		State end = path.get(--size);
+		assertEquals(1, end.getX());
+		assertEquals(1, end.getY());
+		assertEquals(-1, end.getZ());
 	}
 
 }
